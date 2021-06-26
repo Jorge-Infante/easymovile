47e726e494acbeb67574108855f0d876fff43059
Jorge Infante <56237414+Jorge-Infante@users.noreply.github.com>
2021-06-26 01:35:44 -0500

    Restricciones de usuarios

--- easymovil/applications/cars/views.py
+++ easymovil/applications/cars/views.py
@@ -6,6 +6,12 @@ from rest_framework.generics import (
     RetrieveUpdateAPIView
     
     )
+
+from rest_framework.authentication import (
+    TokenAuthentication
+)
+from rest_framework.permissions import (IsAuthenticated,IsAdminUser)
+
 #Modelo car
 from . models import Car
 #Serializador para modelo car
@@ -14,25 +20,40 @@ from . serializers import CarSerializer
 
 class CarCreateApiView(CreateAPIView):
     """Vista para crear objeto car (C)"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = [IsAdminUser]
+    
     serializer_class = CarSerializer
 
     
 class CarListApiView(ListAPIView):
     """Vista para Leer lista de objetos car (R)"""
     serializer_class = CarSerializer
-    
+    #Desencriptar token
+    authentication_classes = (TokenAuthentication,)
+    #tipo de permiso que pose el usuario (usuario razo)
+    permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
+        print('XXXXXXXXXXXXXXXX')
+        print(self.request.user)
         return Car.objects.all()
 
 
 class CarUpdateView(RetrieveUpdateAPIView):
     """Vista para actualizar objeto car (U)"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = [IsAdminUser]
+    
     serializer_class = CarSerializer
     queryset= Car.objects.all()
     
     
 class CarDeleteView(DestroyAPIView):
     """Vista para eliminar objeto car (D)"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = [IsAdminUser]
+        
     serializer_class = CarSerializer
     queryset= Car.objects.all()
 
